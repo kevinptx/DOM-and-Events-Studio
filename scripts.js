71047c88457e4cd212b4255d111fa7164d4fcb87
Kevin Peery <kevin.peery@gmail.com>
2022-08-11 11:28:21 -0500

    update part 1

--- scripts.js
+++ scripts.js
@@ -1,15 +1,16 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 window.onload = function(){
+    const takeOffButton = document.getElementById("takeoff");
     const flightStatus = document.getElementById("flightStatus");
     const shuttleBackground = document.getElementById("shuttleBackground");
     const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
-    if(window.confirm("Confirm that the shuttle is ready for takeoff.")){
-/*  The flight status should change to "Shuttle in flight."
-The background color of the shuttle flight screen (id = "shuttleBackground") should change from green to blue.
- The shuttle height should increase by 10,000 miles. */
-        flightStatus.innerText = "Shuttle in flight.";
-        shuttleBackground.style.background = "dodgerblue";
-        spaceShuttleHeight.innerText = 10000;
-    }
+    
+    takeOffButton.onclick = function(event){
+        if(window.confirm("Confirm that the shuttle is ready for takeoff.")){
+            flightStatus.innerText = "Shuttle in flight.";
+            shuttleBackground.style.background = "dodgerblue";
+            spaceShuttleHeight.innerText = 10000;
+        }
+    };
 };
